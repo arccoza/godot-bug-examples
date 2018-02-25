34f29281c4d81573832b90572368a055bc9e78eb
Adrien <adrien@arccoza.com>
2018-02-26 01:34:55 +0900

    Add minimal example with timer

--- src/scripts/main.gd
+++ src/scripts/main.gd
@@ -1,5 +1,18 @@
 extends Node
 
+var time = 0
+
 
 func _ready():
 	pass
+
+
+func _physics_process(delta):
+	time += delta
+	
+	prints("---", time)
+	if $RayCast2D.is_colliding():
+		prints($RayCast2D.get_collider())
+	
+	if time > 5:
+		$KinematicBody2D.shape_owner_set_disabled(0, true)
